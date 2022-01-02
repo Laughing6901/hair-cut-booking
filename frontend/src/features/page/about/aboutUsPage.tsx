e9fa76b236af9d3d539e980bc84fb6fdc2a44902
Laughing6901 <vandat96tn@gmail.com>
2022-01-02 08:49:57 +0700

    modified image

--- frontend/src/features/page/about/aboutUsPage.tsx
+++ frontend/src/features/page/about/aboutUsPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Header } from "../header/header";
 import { BreadCrumb } from "../breadCrumb/breadCrumb";
 import { AboutUsBody } from "./aboutUsBody";
+import { BarberBody } from "../barber/barberBody";
 
 export const AboutUs:React.FC = () => {
     return(
         <>
             <BreadCrumb tab = "About"/>
             <AboutUsBody/>
+            <BarberBody />
         </>
     )
-}
+}   
